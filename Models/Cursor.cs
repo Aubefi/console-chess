1c00f857e4a11cbdc1d3d91ba9cdb82861f34602
Aubefi <contact.aubefi@gmail.com>
2026-06-10 19:20:51 -0300

    Create Cursor.cs class for the gameplay navigation

---
+++ Models/Cursor.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chess.Models;
+
+public class Cursor(char symbol, ConsoleColor color, byte x, byte y) : UIObject(symbol, color, x, y)
+{
+    public void SetSymbol(char symbol) => Symbol = symbol;
+
+    public void SetX(byte x) => Coordinates = new(x, Coordinates.Y);
+    public void SetY(byte y) => Coordinates = new(Coordinates.X, y);
+}

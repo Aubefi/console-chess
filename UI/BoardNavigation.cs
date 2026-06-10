ae86109a4f2e4d0533fa8025d24dc09b04954eed
Aubefi <contact.aubefi@gmail.com>
2026-06-10 19:22:00 -0300

    Create BoardNavigation.cs for the gameplay's navigation

---
+++ UI/BoardNavigation.cs
@@ -0,0 +1,31 @@
+using System;
+using Chess.Core;
+using Chess.Models;
+
+namespace Chess.UI;
+
+public class BoardNavigation
+{
+    public static void ReadPlayerInput(Board board, Cursor cursor)
+    {
+        var keyInfo = Console.ReadKey(true);
+
+        switch (keyInfo.Key)
+        {
+            case ConsoleKey.UpArrow:
+                if (cursor.Coordinates.Y > 0) cursor.SetY((byte)(cursor.Coordinates.Y - 1));
+                break;
+            case ConsoleKey.LeftArrow:
+                if (cursor.Coordinates.X > 0) cursor.SetX((byte)(cursor.Coordinates.X - 1));
+                break;
+            case ConsoleKey.RightArrow:
+                if (cursor.Coordinates.X < 7) cursor.SetX((byte)(cursor.Coordinates.X + 1));
+                break;
+            case ConsoleKey.DownArrow:
+                if (cursor.Coordinates.Y < 7) cursor.SetY((byte)(cursor.Coordinates.Y + 1));
+                break;
+            default:
+                break;
+        }
+    }
+}

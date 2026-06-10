d9659da3d2c5abf52609c4c85d89085eafa80667
Aubefi <contact.aubefi@gmail.com>
2026-06-10 19:25:56 -0300

    Update Program.cs to initialize the core game

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Chess;
+using System;
+using Chess.Core;
+
+namespace Chess;
 
 public sealed class Program
 {
     public static void Main()
     {
+        Game game = new();
 
+        game.Start();
+        game.Update();
+
+        Console.CursorVisible = true;
     }
 }

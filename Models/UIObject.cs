33b49a523923f79f1e4ad244b0ba6593e20771e4
Aubefi <contact.aubefi@gmail.com>
2026-06-10 18:53:47 -0300

    Create abstract UIObject.cs class

---
+++ Models/UIObject.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Chess.Models;
+
+public abstract class UIObject(char symbol, ConsoleColor color, byte x, byte y)
+{
+    public char Symbol { get; protected set; } = symbol;
+
+    public ConsoleColor Color { get; protected set; } = color;
+
+    public Position Coordinates { get; protected set; } = new Position(x, y);
+
+    public record struct Position(byte X, byte Y);
+}

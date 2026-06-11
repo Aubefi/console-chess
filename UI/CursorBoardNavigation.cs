878dfe7e197b5a9e9887408f2899e5dff8530d4a
Aubefi <contact.aubefi@gmail.com>
2026-06-11 16:06:39 -0300

    Refactor CursorBoardNavigation.cs
    - Change was made to match current project infrastructure, as seen with the
      inclusion of BaseBehavior.cs.

--- UI/CursorBoardNavigation.cs
+++ UI/CursorBoardNavigation.cs
@@ -1,13 +1,17 @@
 using System;
+using Chess.Engine;
 using Chess.Objects;
 
 namespace Chess.UI;
 
-public class CursorBoardNavigation
+public class CursorBoardNavigation : BaseBehavior
 {
+    public required Board Board { get; set; }
+    public required Cursor Cursor { get; set; }
+
     public static event Action<BaseUIObject>? PlayerSelectedPieceEvent;
 
-    public static void ReadPlayerInput(Board board, Cursor cursor)
+    public override void Update()
     {
         var keyInfo = Console.ReadKey(true);
 
@@ -15,27 +19,27 @@ public class CursorBoardNavigation
         {
             case ConsoleKey.UpArrow:
             case ConsoleKey.W:
-                if (cursor.Coordinates.Y > 0) cursor.SetY((byte)(cursor.Coordinates.Y - 1));
-                break;
-
-            case ConsoleKey.LeftArrow:
-            case ConsoleKey.A:
-                if (cursor.Coordinates.X > 0) cursor.SetX((byte)(cursor.Coordinates.X - 1));
-                break;
-
-            case ConsoleKey.RightArrow:
-            case ConsoleKey.D:
-                if (cursor.Coordinates.X < 7) cursor.SetX((byte)(cursor.Coordinates.X + 1));
+                if (Cursor.Coordinates.Y > 0) Cursor.SetY((byte)(Cursor.Coordinates.Y - 1));
                 break;
 
             case ConsoleKey.DownArrow:
             case ConsoleKey.S:
-                if (cursor.Coordinates.Y < 7) cursor.SetY((byte)(cursor.Coordinates.Y + 1));
+                if (Cursor.Coordinates.Y < 7) Cursor.SetY((byte)(Cursor.Coordinates.Y + 1));
+                break;
+
+            case ConsoleKey.LeftArrow:
+            case ConsoleKey.A:
+                if (Cursor.Coordinates.X > 0) Cursor.SetX((byte)(Cursor.Coordinates.X - 1));
+                break;
+
+            case ConsoleKey.RightArrow:
+            case ConsoleKey.D:
+                if (Cursor.Coordinates.X < 7) Cursor.SetX((byte)(Cursor.Coordinates.X + 1));
                 break;
 
             case ConsoleKey.Spacebar:
             case ConsoleKey.Enter:
-                var piece = board.GetPieceByCoordinates(cursor.Coordinates);
+                var piece = Board.GetPieceByCoordinates(Cursor.Coordinates);
                 PlayerSelectedPieceEvent?.Invoke(piece);
                 break;
 

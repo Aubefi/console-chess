1e79608b9be968b8dc826da2246786ae404edf17
Aubefi <contact.aubefi@gmail.com>
2026-06-11 11:23:10 -0300

    New HasMoved property for Pawn.cs

--- Objects/Pawn.cs
+++ Objects/Pawn.cs
@@ -4,4 +4,5 @@ namespace Chess.Objects;
 
 public class Pawn(char symbol, ConsoleColor color, byte x, byte y) : BaseUIObject(symbol, color, x, y)
 {
+    public bool HasMoved { get; set; } = false;
 }

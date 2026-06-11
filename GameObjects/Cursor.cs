51c85e479354bf2e5460ac849baca27c83b20c55
Aubefi <contact.aubefi@gmail.com>
2026-06-11 19:02:18 -0300

    Refactor Cursor.cs to be able to select pieces and move around

--- GameObjects/Cursor.cs
+++ GameObjects/Cursor.cs
@@ -1,8 +1,39 @@
 using System;
+using Chess.UI;
 
 namespace Chess.Objects;
 
-public class Cursor(char symbol, ConsoleColor color, byte x, byte y) : BaseUIObject(symbol, color, x, y)
+public class Cursor : BaseUIObject
 {
-    public void SetSymbol(char symbol) => Symbol = symbol;
+    public Cursor(char symbol, ConsoleColor color, byte x, byte y) : base(symbol, color, x, y)
+    {
+        CursorBoardNavigation.PlayerSelectedPieceEvent += OnPlayerSelectedPieceEvent;
+    }
+
+    public bool IsHoldingPiece { get; set; }
+
+    public ConsoleColor BackgroundColor { get; set; } = ConsoleColor.Blue;
+
+    private void OnPlayerSelectedPieceEvent(BaseUIObject piece)
+    {
+        if (IsHoldingPiece)
+        {
+            IsHoldingPiece = false;
+            BackgroundColor = ConsoleColor.Blue;
+        }
+        else if (piece is not EmptySquare)
+        {
+            Symbol = piece.Symbol;
+            BackgroundColor = ConsoleColor.Yellow;
+            IsHoldingPiece = true;
+        }
+    }
+
+    public void SetSymbol(char symbol)
+    {
+        if (IsHoldingPiece is false)
+        {
+            Symbol = symbol;
+        }
+    }
 }

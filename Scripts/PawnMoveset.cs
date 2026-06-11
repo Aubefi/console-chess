e7dbc3cb617a95761a4f1bd7c64b18623f941f94
Aubefi <contact.aubefi@gmail.com>
2026-06-11 11:17:36 -0300

    Create PawnMoveset.cs

---
+++ Scripts/PawnMoveset.cs
@@ -0,0 +1,24 @@
+using System;
+using Chess.Objects;
+
+namespace Chess.Scripts;
+
+public class PawnMoveset()
+{
+    public static void MovePawn(Pawn pawn, Position targetPos)
+    {
+        var distance = pawn.HasMoved ? 1 : 2;
+
+        var isMoveAllowed = pawn.Color switch
+        {
+            ConsoleColor.White => targetPos.Y >= pawn.Coordinates.Y - distance,
+            ConsoleColor.Gray => targetPos.Y <= pawn.Coordinates.Y + distance,
+            _ => false
+        };
+
+        if (isMoveAllowed is false)
+            return;
+
+        pawn.SetPosition(targetPos);
+    }
+}

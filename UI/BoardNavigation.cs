762e636aed116f9ceb56de0e37576f9ee0c1c04c
Aubefi <contact.aubefi@gmail.com>
2026-06-11 11:19:23 -0300

    Add new movement control keys in BoardNavigation.cs

--- UI/BoardNavigation.cs
+++ UI/BoardNavigation.cs
@@ -12,15 +12,22 @@ public class BoardNavigation
         switch (keyInfo.Key)
         {
             case ConsoleKey.UpArrow:
+            case ConsoleKey.W:
                 if (cursor.Coordinates.Y > 0) cursor.SetY((byte)(cursor.Coordinates.Y - 1));
                 break;
+
             case ConsoleKey.LeftArrow:
+            case ConsoleKey.A:
                 if (cursor.Coordinates.X > 0) cursor.SetX((byte)(cursor.Coordinates.X - 1));
                 break;
+
             case ConsoleKey.RightArrow:
+            case ConsoleKey.D:
                 if (cursor.Coordinates.X < 7) cursor.SetX((byte)(cursor.Coordinates.X + 1));
                 break;
+
             case ConsoleKey.DownArrow:
+            case ConsoleKey.S:
                 if (cursor.Coordinates.Y < 7) cursor.SetY((byte)(cursor.Coordinates.Y + 1));
                 break;
             default:

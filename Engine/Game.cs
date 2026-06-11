16d27d9b632ae39b010c4a339694abf9da451bc7
Aubefi <contact.aubefi@gmail.com>
2026-06-11 16:07:11 -0300

    Refactor Game.cs to manage the game's entire structure

--- Engine/Game.cs
+++ Engine/Game.cs
@@ -1,34 +1,58 @@
 using System;
+using System.Collections.Generic;
 using Chess.Objects;
 using Chess.UI;
 
 namespace Chess.Engine;
 
-public sealed class Game()
+public sealed class Game : BaseObject
 {
-    private readonly Board _board = new();
-    private readonly Cursor _cursor = new(' ', ConsoleColor.Gray, 0, 0);
+    private readonly List<BaseObject> _baseObjects = [];
+    private readonly List<BaseBehavior> _baseBehaviors = [];
 
-    public void Start()
+    private readonly Cursor _cursor;
+    private readonly Board _board;
+
+    private readonly CursorBoardNavigation _cursorBoardNavigation;
+
+    private readonly Start _start;
+    private readonly Update _update;
+    private readonly Finish _finish;
+
+    public Game()
     {
-        ConfigureConsole();
+        _cursor = new(' ', ConsoleColor.Gray, 0, 0);
+        _board = new Board { BoardCursor = _cursor };
 
-        _board.BuildBaseGridObjects();
+        _cursorBoardNavigation = new CursorBoardNavigation { Board = _board, Cursor = _cursor };
+
+        _start = new Start { BaseBehaviors = _baseBehaviors };
+        _update = new Update { BaseBehaviors = _baseBehaviors };
+        _finish = new Finish { BaseBehaviors = _baseBehaviors };
+
+        InitializeBaseObjects();
+        InitializeBaseBehaviors();
     }
 
-    public void Update()
+    public override void Run()
     {
-        while (true)
+        foreach (var obj in _baseObjects)
         {
-            _board.DisplayGridObjects(_cursor);
-            BoardNavigation.ReadPlayerInput(_board, _cursor);
+            obj.Run();
         }
     }
 
-    private static void ConfigureConsole()
+    private void InitializeBaseObjects()
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.CursorVisible = false;
-        Console.Clear();
+        _baseObjects.Add(_start);
+        _baseObjects.Add(_update);
+        _baseObjects.Add(_finish);
+    }
+
+    private void InitializeBaseBehaviors()
+    {
+        _baseBehaviors.Add(_board);
+        _baseBehaviors.Add(_cursor);
+        _baseBehaviors.Add(_cursorBoardNavigation);
     }
 }

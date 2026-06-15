af129d0fd07f3f6c0049b8833a129d09fecc0978
Aubefi <contact.aubefi@gmail.com>
2026-06-15 17:51:44 -0300

    Refactor BaseUIObject.cs to include every SetProperty method

--- Engine/Bases/BaseUIObject.cs
+++ Engine/Bases/BaseUIObject.cs
@@ -7,13 +7,14 @@ public record struct Position(byte X, byte Y);
 public abstract class BaseUIObject(char symbol, ConsoleColor color, byte x, byte y)
 {
     public char Symbol { get; protected set; } = symbol;
+    public virtual void SetSymbol(char symbol)
+        => Symbol = symbol;
 
     public ConsoleColor Color { get; protected set; } = color;
+    public virtual void SetColor(ConsoleColor color)
+        => Color = color;
 
     public Position Pos { get; protected set; } = new Position(x, y);
     public virtual void SetPosition(Position pos)
         => Pos = pos;
-
-    public virtual void SetSymbol(char symbol)
-        => Symbol = symbol;
 }

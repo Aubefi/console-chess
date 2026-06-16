2d9f43496433d33c19b8326fbd75a5682bd952ab
Aubefi <contact.aubefi@gmail.com>
2026-06-15 23:25:07 -0300

    Refactor ChessPiece.cs to include color factor
    - It is used for the position calculation of the piece.

--- Assets/ChessPieces/ChessPiece.cs
+++ Assets/ChessPieces/ChessPiece.cs
@@ -1,8 +1,20 @@
 using System;
+using System.Collections.Generic;
 using Chess.Engine.Bases;
 
 namespace Chess.Assets.ChessPieces;
 
+public enum ChessPieceColorFactor
+{
+    Black = 1,
+    White = -1
+}
+
 public abstract class ChessPiece(char symbol, ConsoleColor color, byte x, byte y) : BaseUIObject(symbol, color, x, y)
 {
+    public abstract ChessPieceColorFactor ColorFactor { get; protected set; }
+    public virtual void SetColorFactor(ChessPieceColorFactor colorFactor)
+        => ColorFactor = colorFactor;
+
+    protected abstract List<Position> GetAllowedSquares(BaseUIObject[,] objects);
 }

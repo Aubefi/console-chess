7d03eb760a44007424e0dbab3185b5c016d8266e
Aubefi <contact.aubefi@gmail.com>
2026-06-16 12:59:55 -0300

    Update chess pieces subclasses to implement new parent members

--- Assets/ChessPieces/Knight.cs
+++ Assets/ChessPieces/Knight.cs
@@ -1,7 +1,15 @@
 using System;
+using System.Collections.Generic;
+using Chess.Engine.Bases;
 
 namespace Chess.Assets.ChessPieces;
 
 public class Knight(char symbol, ConsoleColor color, int x, int y) : ChessPiece(symbol, color, x, y)
 {
+    public override ChessPieceColorFactor ColorFactor { get; protected set; }
+
+    public override List<Position> GetAllowedSquares(BaseUIObject[,] objects)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Assets/ChessPieces/Pawn.cs
+++ Assets/ChessPieces/Pawn.cs
@@ -1,8 +1,36 @@
 using System;
+using System.Collections.Generic;
+using Chess.Engine.Bases;
 
 namespace Chess.Assets.ChessPieces;
 
-public class Pawn(char symbol, ConsoleColor color, byte x, byte y) : ChessPiece(symbol, color, x, y)
+public class Pawn(char symbol, ConsoleColor color, int x, int y) : ChessPiece(symbol, color, x, y)
 {
     public bool HasMoved { get; set; } = false;
+
+    public override ChessPieceColorFactor ColorFactor { get; protected set; }
+
+    private List<Position> _allowedSquares = [];
+
+    public override List<Position> GetAllowedSquares(BaseUIObject[,] objects)
+    {
+        _allowedSquares = [];
+
+        TryAddPosition(objects, new Position(Pos.X, Pos.Y + (int)ColorFactor));
+
+        if (!HasMoved)
+        {
+            TryAddPosition(objects, new Position(Pos.X, Pos.Y + 2 * (int)ColorFactor));
+        }
+
+        return _allowedSquares;
+    }
+
+    private void TryAddPosition(BaseUIObject[,] objects, Position pos)
+    {
+        if (objects[pos.Y, pos.X] is not ChessPiece)
+        {
+            _allowedSquares.Add(pos);
+        }
+    }
 }

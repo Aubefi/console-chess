75433a36e20e0fea1532187ac8fe4696867c9ab7
Aubefi <contact.aubefi@gmail.com>
2026-06-16 13:02:02 -0300

    Set background color to be a inherited property in parent class

--- Engine/Bases/BaseCursor.cs
+++ Engine/Bases/BaseCursor.cs
@@ -1,11 +1,7 @@
 using System;
-using Chess.Assets;
 
 namespace Chess.Engine.Bases;
 
 public class BaseCursor(char symbol, ConsoleColor color, int x, int y) : BaseUIObject(symbol, color, x, y)
 {
-    public ConsoleColor BackgroundColor { get; protected set; } = Colors.Cursor["Default"];
-    public virtual void SetBackgroundColor(ConsoleColor color)
-        => BackgroundColor = color;
 }
--- Engine/Bases/BaseUIObject.cs
+++ Engine/Bases/BaseUIObject.cs
@@ -14,6 +14,12 @@ public abstract class BaseUIObject(char symbol, ConsoleColor color, int x, int y
     public virtual void SetColor(ConsoleColor color)
         => Color = color;
 
+    public ConsoleColor? BackgroundColor { get; protected set; } = null;
+    public virtual void SetBackgroundColor(ConsoleColor color)
+        => BackgroundColor = color;
+    public virtual void SetBackgrounColorToNull()
+        => BackgroundColor = null;
+
     public Position Pos { get; protected set; } = new Position(x, y);
     public virtual void SetPosition(Position pos)
         => Pos = pos;

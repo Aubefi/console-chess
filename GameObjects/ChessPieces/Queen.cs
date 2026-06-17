c441b4df8b7dc7cff61fddc64a33ee5339c87ad2
Aubefi <contact.aubefi@gmail.com>
2026-06-17 16:00:34 -0300

    Implement queen movement in Queen.cs

--- GameObjects/ChessPieces/Queen.cs
+++ GameObjects/ChessPieces/Queen.cs
@@ -8,6 +8,54 @@ public class Queen(char symbol, ConsoleColor color, int x, int y) : ChessPiece(s
 {
     public override List<Position> GetLegalMoves(BaseUIObject[,] boardObjects)
     {
-        throw new NotImplementedException();
+        var queenMoves = CalculateQueenMoves(boardObjects);
+
+        queenMoves.RemoveAll(p => !IsPositionInsideBoard(p));
+
+        return queenMoves;
+    }
+
+    private List<Position> CalculateQueenMoves(BaseUIObject[,] boardObjects)
+    {
+        var list = new List<Position>();
+
+        var a = (x: 1, y: 0);
+        var b = (x: 1, y: 1);
+
+        for (int k = 0; k < 4; k++)
+        {
+            var posA = new Position(Pos.X + a.x, Pos.Y + a.y);
+            var posB = new Position(Pos.X + b.x, Pos.Y + b.y);
+
+            var sumAX = a.x;
+            var sumAY = a.y;
+            var sumBX = b.x;
+            var sumBY = b.y;
+
+            while (!IsPositionOccupied(boardObjects, posA))
+            {
+                list.Add(posA);
+
+                sumAX += a.x;
+                sumAY += a.y;
+
+                posA = new(Pos.X + sumAX, Pos.Y + sumAY);
+            }
+
+            while (!IsPositionOccupied(boardObjects, posB))
+            {
+                list.Add(posB);
+
+                sumBX += b.x;
+                sumBY += b.y;
+
+                posB = new(Pos.X + sumBX, Pos.Y + sumBY);
+            }
+
+            (a.x, a.y) = (-a.y, a.x);
+            (b.x, b.y) = (-b.y, b.x);
+        }
+
+        return list;
     }
 }

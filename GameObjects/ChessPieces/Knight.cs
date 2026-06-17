39cf8bea26c064cae4414b92e1bfdf0f531675f0
Aubefi <contact.aubefi@gmail.com>
2026-06-17 16:00:47 -0300

    Implement knight movement in Knight.cs

--- GameObjects/ChessPieces/Knight.cs
+++ GameObjects/ChessPieces/Knight.cs
@@ -8,6 +8,29 @@ public class Knight(char symbol, ConsoleColor color, int x, int y) : ChessPiece(
 {
     public override List<Position> GetLegalMoves(BaseUIObject[,] boardObjects)
     {
-        throw new NotImplementedException();
+        var knightMoves = CalculateKnightMoves(boardObjects);
+
+        knightMoves.RemoveAll(p => !IsPositionInsideBoard(p));
+
+        return knightMoves;
+    }
+
+    private List<Position> CalculateKnightMoves(BaseUIObject[,] boardObjects)
+    {
+        var list = new List<Position>();
+
+        var a = (x: 1, y: 2);
+        var b = (x: 2, y: 1);
+
+        for (int k = 0; k < 4; k++)
+        {
+            (a.x, a.y) = (-a.y, a.x);
+            list.Add(new(Pos.X + a.x, Pos.Y + a.y));
+
+            (b.x, b.y) = (-b.y, b.x);
+            list.Add(new(Pos.X + b.x, Pos.Y + b.y));
+        }
+
+        return list;
     }
 }

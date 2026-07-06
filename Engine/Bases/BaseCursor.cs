e8360901ef559720326fb46663ccccc6cabd7628
Aubefi <contact.aubefi@gmail.com>
2026-07-06 20:49:31 -0300

    Refactor BaseCursor to become abstract
    - I forgot.

--- Engine/Bases/BaseCursor.cs
+++ Engine/Bases/BaseCursor.cs
@@ -2,6 +2,6 @@ using System;
 
 namespace Chess.Engine.Bases;
 
-public class BaseCursor(char symbol, ConsoleColor color, int x, int y) : BaseUIObject(symbol, color, x, y)
+public abstract class BaseCursor(char symbol, ConsoleColor color, int x, int y) : BaseUIObject(symbol, color, x, y)
 {
 }

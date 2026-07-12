664e1f0312cc995f6c5e7d51ed2c7e518441a600
Aubefi <contact.aubefi@gmail.com>
2026-07-12 00:38:55 -0300

    Make some properties of BaseUIObject be virtual

--- Engine/Bases/BaseUIObject.cs
+++ Engine/Bases/BaseUIObject.cs
@@ -6,15 +6,15 @@ public record struct Position(int X, int Y);
 
 public abstract class BaseUIObject(char symbol, ConsoleColor color, int x, int y)
 {
-    public char Symbol { get; protected set; } = symbol;
+    public virtual char Symbol { get; protected set; } = symbol;
     public virtual void SetSymbol(char symbol)
         => Symbol = symbol;
 
-    public ConsoleColor Color { get; protected set; } = color;
+    public virtual ConsoleColor Color { get; protected set; } = color;
     public virtual void SetColor(ConsoleColor color)
         => Color = color;
 
-    public ConsoleColor? BackgroundColor { get; protected set; } = null;
+    public virtual ConsoleColor? BackgroundColor { get; protected set; } = null;
     public virtual void SetBackgroundColor(ConsoleColor color)
         => BackgroundColor = color;
     public virtual void RemoveBackgroundColor()

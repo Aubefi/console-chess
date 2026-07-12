fd5a20146302932eb9a0a30bcc6610ba6b81515a
Aubefi <contact.aubefi@gmail.com>
2026-07-12 00:40:33 -0300

    Improves readability in BaseCursor.cs

--- Engine/Bases/BaseCursor.cs
+++ Engine/Bases/BaseCursor.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace Chess.Engine.Bases;
 
-public abstract class BaseCursor(char symbol, ConsoleColor color, int x, int y) : BaseUIObject(symbol, color, x, y)
+public abstract class BaseCursor(char symbol, ConsoleColor color, int x, int y)
+: BaseUIObject(symbol, color, x, y)
 {
 }

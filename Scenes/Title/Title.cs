00cc41ed5eaeaa0869f545bda0acff88fa6322c0
Aubefi <contact.aubefi@gmail.com>
2026-07-22 10:40:45 -0300

    Update Title.cs to use new title related classes

--- Scenes/Title/Title.cs
+++ Scenes/Title/Title.cs
@@ -1,16 +1,32 @@
+using Chess.Assets;
 using Chess.Engine.Bases;
+using Chess.Scenes.Gameplay;
 
 namespace Chess.Scenes.Title;
 
 public sealed class Title : BaseScene
 {
+    private readonly TitleCursor _titleCursor = new(char.MinValue, Colors.Cursor["Foreground"], 0, 0);
+    private readonly TitleRenderer _titleRenderer = new();
+    private readonly TitleInputs _titleInputs = new();
+
+    public Title()
+    {
+        _titleCursor.SetBackgroundColor(Colors.Cursor["Background"]);
+
+        InitializeBehaviors();
+        InitializeDependencies();
+    }
+
     protected override void InitializeBehaviors()
     {
-        throw new System.NotImplementedException();
+        Behaviors.Add(_titleRenderer);
+        Behaviors.Add(_titleInputs);
     }
 
     protected override void InitializeDependencies()
     {
-        throw new System.NotImplementedException();
+        _titleRenderer.Cursor = _titleCursor;
+        _titleInputs.Cursor = _titleCursor;
     }
 }

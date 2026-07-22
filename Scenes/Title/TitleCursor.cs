f76f56fae3da46dca12a2326ae2a3d07c5941f17
Aubefi <contact.aubefi@gmail.com>
2026-07-22 10:39:28 -0300

    Create TitleCursor.cs

---
+++ Scenes/Title/TitleCursor.cs
@@ -0,0 +1,31 @@
+using System;
+using Chess.Engine.Bases;
+
+namespace Chess.Scenes.Gameplay;
+
+public sealed class TitleCursor : BaseCursor
+{
+    public override Position Pos
+    {
+        get;
+        protected set
+        {
+            LastPosition = field;
+            field = value;
+        }
+    }
+
+    public Position LastPosition { get; set; }
+
+    public TitleCursor(char symbol, ConsoleColor color, int x, int y) : base(symbol, color, x, y)
+    {
+        Pos = new(x, y);
+        LastPosition = Pos;
+    }
+
+    public bool HasMoved
+    {
+        get => field = LastPosition != Pos;
+        private set;
+    }
+}
